921edf79169af44cd832356bb1749a3b87699fe6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-02 15:21:44 -0300

    implement currencyAdjustmentJob

--- src/main/java/me/calebe_oliveira/intermediatespringbatchapp/config/BankTransactionConfiguration.java
+++ src/main/java/me/calebe_oliveira/intermediatespringbatchapp/config/BankTransactionConfiguration.java
@@ -7,6 +7,7 @@ import me.calebe_oliveira.intermediatespringbatchapp.model.MerchantMonthBalance;
 import me.calebe_oliveira.intermediatespringbatchapp.processors.FillBalanceProcessor;
 import me.calebe_oliveira.intermediatespringbatchapp.utils.SourceManagementUtils;
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.ItemReadListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
@@ -15,6 +16,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
@@ -23,14 +25,20 @@ import org.springframework.batch.item.json.JsonFileItemWriter;
 import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.batch.BatchDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.WritableResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Configuration
 @Import(SourceConfiguration.class)//maybe it's not needed?
@@ -158,5 +166,96 @@ public class BankTransactionConfiguration {
                 .build();
     }
 
+    // Now Currency Adjustment Job
+    private static class CurrencyAdjustment {
+        long id;
+        BigDecimal adjustedAmount;
+    }
 
+    @Bean
+    @Qualifier("currencyAdjustmentJob")
+    public Job currencyAdjustmentJob(JobRepository jobRepository,
+                                     @Qualifier("currencyAdjustmentStep") Step currencyAdjustmentStep) {
+        return new JobBuilder("currencyAdjustmentJob", jobRepository)
+                .start(currencyAdjustmentStep)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("currencyAdjustmentStep")
+    public Step currencyAdjustmentStep(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+                                       @Qualifier("sourceDataSource") DataSource sourceDataSource,
+                                       @Value("${currency.adjustment.rate}") double rate,
+                                       @Value("${currency.adjustment.disallowed.merchant}") String disallowedMerchant) {
+        return new StepBuilder("currency-adjustment", jobRepository)
+                .<BankTransaction, CurrencyAdjustment>chunk(1, transactionManager)
+                .reader(new JdbcCursorItemReaderBuilder<BankTransaction>()
+                        .dataSource(sourceDataSource)
+                        .name("bankTransactionReader")
+                        .sql(BankTransaction.SELECT_ALL_QUERY + " WHERE adjusted = false")
+                        .saveState(false)
+                        .build())
+                .processor(item -> {
+                    CurrencyAdjustment adjustment = new CurrencyAdjustment();
+                    adjustment.id = item.getId();
+                    adjustment.adjustedAmount = item.getAmount()
+                            .multiply(BigDecimal.valueOf(rate))
+                            .setScale(2, RoundingMode.HALF_UP);
+                    return adjustment;
+                })
+                .writer(new JdbcBatchItemWriterBuilder<CurrencyAdjustment>()
+                        .dataSource(sourceDataSource)
+                        .itemPreparedStatementSetter((item, ps) -> {
+                            ps.setBigDecimal(1, item.adjustedAmount);
+                            ps.setBoolean(2, true);
+                            ps.setLong(3, item.id);
+                        })
+                        .sql("UPDATE bank_transaction_yearly SET amount = ?, adjusted = ? WHERE id = ?")
+                        .build()
+                )
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(StepExecution stepExecution) {
+                        SourceManagementUtils.addAdjustedColumn(sourceDataSource);
+                    }
+                })
+                .listener(new ItemReadListener<>() {
+                    @Override
+                    public void afterRead(BankTransaction item) {
+                       if(disallowedMerchant.equalsIgnoreCase(item.getMerchant())) {
+                           throw new RuntimeException("Disallowed Merchant!");
+                       }
+                    }
+                })
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+
+    @Bean
+    @Qualifier("dataSource")
+    public DataSource dataSource(@Value("${db.job.repo.url}") String url,
+                                 @Value("${db.job.repo.username}") String username,
+                                 @Value("${db.job.repo.password}") String password) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        JdbcTransactionManager transactionManager = new JdbcTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+
+    @Bean
+    public BatchDataSourceScriptDatabaseInitializer batchDataSourceScriptDatabaseInitializer(@Qualifier("dataSource") DataSource dataSource,
+                                                                                             BatchProperties batchProperties) {
+        return new BatchDataSourceScriptDatabaseInitializer(dataSource, batchProperties.getJdbc());
+    }
 }

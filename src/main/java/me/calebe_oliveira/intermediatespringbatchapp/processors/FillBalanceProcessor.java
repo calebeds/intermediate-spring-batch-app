9d6e429d0c7ff6dede8a9000cb9b4bb50d52c9c5
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-02 09:58:10 -0300

    add FillBalanceProcessor to processors package

---
+++ src/main/java/me/calebe_oliveira/intermediatespringbatchapp/processors/FillBalanceProcessor.java
@@ -0,0 +1,39 @@
+package me.calebe_oliveira.intermediatespringbatchapp.processors;
+
+import me.calebe_oliveira.intermediatespringbatchapp.model.BalanceUpdate;
+import me.calebe_oliveira.intermediatespringbatchapp.model.BankTransaction;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class FillBalanceProcessor implements ItemProcessor<BankTransaction, BalanceUpdate> {
+    public static final String BALANCE_SO_FAR = "balanceSoFar";
+    private StepExecution stepExecution;
+
+    public void setStepExecution(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
+    }
+
+    @Override
+    public BalanceUpdate process(BankTransaction item) throws Exception {
+        if(stepExecution == null) {
+            throw new RuntimeException("Can not process item without accessing the step execution");
+        }
+        BigDecimal newBalance = BigDecimal.valueOf(getLatestTransactionBalance())
+                .setScale(2, RoundingMode.HALF_UP)
+                .add(item.getAmount());
+        BalanceUpdate balanceUpdate = new BalanceUpdate(item.getId(), newBalance);
+        stepExecution.getExecutionContext().putDouble(BALANCE_SO_FAR, newBalance.doubleValue());
+        return balanceUpdate;
+    }
+
+    private double getLatestTransactionBalance() {
+        if(stepExecution == null) {
+            throw new RuntimeException("Can not process item without accessing the step execution");
+        }
+
+        return stepExecution.getExecutionContext().getDouble(BALANCE_SO_FAR, 0d);
+    }
+}

92ea6da8fdb1b89ab2086b426be0cb69bb511a9c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-01 21:21:54 -0300

    first commit

---
+++ src/main/java/me/calebe_oliveira/intermediatespringbatchapp/IntermediateSpringBatchAppApplication.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.intermediatespringbatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermediateSpringBatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermediateSpringBatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/calebe_oliveira/intermediatespringbatchapp/IntermediateSpringBatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.intermediatespringbatchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntermediateSpringBatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
